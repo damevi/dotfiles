c2a19c8d5934932a31f4cc9102c7db44809a299a
damevi <maciej.urlo@gmail.com>
2024-12-17 15:06:03 +0100

    update alacritty and fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,3 +9,6 @@ set -x PATH $DENO_INSTALL/bin:$PATH
 
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
+
+# Added by `rbenv init` on Tue Dec 17 14:56:40 CET 2024
+status --is-interactive; and rbenv init - --no-rehash fish | source
