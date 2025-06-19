8be923fab3c3aba567f944b7603c8ba52f096423
damevi <maciej.urlo@gmail.com>
2025-06-19 17:02:56 +0200

    add zsh aliases

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,22 @@
+# Git
+alias g='git'
+alias ga='git add -A && git commit -m'
+alias gpo='git push origin'
+alias gp='git push'
+alias gf='git fetch'
+alias gs='git status'
+alias gss='git status -s'
+alias glo='git pull origin'
+alias gl='git pull'
+alias gd='git diff'
+alias gm='git merge'
+
+# Folders
+alias doc="$HOME/Documents"
+alias dow="$HOME/Downloads"
+
+# Better ls
+alias ls="eza --all --icons=always"
+
+# Lazygit
+alias lg="lazygit"
---
+++ dot_config/zsh/custom.zsh
@@ -0,0 +1,14 @@
+# Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+export HOMEBREW_NO_AUTO_UPDATE=1
+
+
+# Starship
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+eval "$(starship init zsh)"
+starship config palette $STARSHIP_THEME
+
+# Load Git completion
+zstyle ':completion:*:*:git:*' script $HOME/.config/zsh/git-completion.bash
+fpath=($HOME/.config/zsh $fpath)
+autoload -Uz compinit && compinit
