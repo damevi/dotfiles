5d0ac2386a565c4945a2d0586f4e5250fd816ba7
damevi <maciej.urlo@gmail.com>
2025-11-24 12:49:22 +0100

    update nvim keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,5 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center on page up" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center on page down" })
 vim.keymap.set({ "n", "x", "o" }, "L", "$", { desc = "Move to the end of the line" })
 vim.keymap.set({ "n", "x", "o" }, "H", "^", { desc = "Move to the beginning of the line" })
+
+vim.keymap.set("n", "<leader><leader>", "<cmd>e #<cr>", { desc = "Switch to previous buffer" })
